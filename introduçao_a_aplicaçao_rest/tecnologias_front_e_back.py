825cc1d0e6ad5dee72dbfc9c07040abaacbbbe79
Aiko <mutantetrakor82@gmail.com>
2025-03-16 18:27:12 -0300

    primeiras aulas conceito

---
+++ introduçao_a_aplicaçao_rest/tecnologias_front_e_back.py
@@ -0,0 +1,12 @@
+"""
+front end refere-se a parte do desvolvimento web que lida com a iterface
+do usuario. o objetivo é apresentar informaçoes de forma interativa e acessivel para o usuario final
+
+tecnologias chave 
+HTML (hypertext markup language): estrutura o conteudo da web
+
+CSS
+
+
+
+"""
