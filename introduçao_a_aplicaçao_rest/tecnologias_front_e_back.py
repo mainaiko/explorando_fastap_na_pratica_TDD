08907b15544fdc9e4e12f8a93495f0e5615f23b3
Aiko <mutantetrakor82@gmail.com>
2025-03-25 11:13:06 -0300

    atualizaçao aulas

--- introduçao_a_aplicaçao_rest/tecnologias_front_e_back.py
+++ introduçao_a_aplicaçao_rest/tecnologias_front_e_back.py
@@ -5,8 +5,22 @@ do usuario. o objetivo é apresentar informaçoes de forma interativa e acessive
 tecnologias chave 
 HTML (hypertext markup language): estrutura o conteudo da web
 
-CSS
+CSS (cascading style sheets): estiliza e apresenta o conteudo HTML
 
+JavaScript: torna as paginas web interativas e dinamicas
 
+back ed a logica por tras dos bastidores, a parte do site que o usuario nao ve. inclui servidor, aplicaçao e banco de dados
+garante que tudo no front funcione corretamente
 
+tecnologias chave
+python, ruby, php, java
+
+banco de dados
+postgreSQL, MySQL, MongoDB, Oracle
+
+frameworks
+Django(python), spring boot(java)
+
+desemvolvimento full stack
+sao profissionais que tem habilidades tanto em front quanto em back
 """
