0914775a1fd04bcd407fdbc9cb9ea229a50ebc1e
Aiko <mutantetrakor82@gmail.com>
2025-03-30 11:03:10 -0300

    atualizaçoes aulas

---
+++ introduçao_a_aplicaçao_rest/tipos_de_APIs.py
@@ -0,0 +1,27 @@
+"""
+API RESTful
+RESTful refere-se a APIs que segues principios do REST (representational state transfer). sao baseadas
+em padroes HTTP e utilizadas para interaçoes na web
+caracteristicas:
+uso de metodos HTTP (get, post, put, delete) para operaçoes CRUD
+curva de aprendizado menor
+facil de entender e implementar
+
+CRUD = creat read update delete
+
+API SOAP
+SOAP (simple object access protocol) é um protocolo que define um padrao para a troca de mensagens baseadas em XML
+caracteristicas:
+protocolo baseado em XML para troca de informaçoes
+independente de linguagem e plataforma de transporte
+suporte para operaçoes complexas e segurança avançada
+
+API GraphQ
+uma linguagem de consulta para sua API e um servidor capaz de executar essas consultas retornando apenas dados especificados
+caracteristicas:
+permite que os clientes especifiquem exatamente quais dados querem
+eficiente na reduçao de solicitaçoes e no tamanho dos dados transferido
+flexivel e fortemente tipada, facilitando a evoluçao das APIs
+
+
+"""
