095fb46c81c23b43bb39945d993c3d1c9899bab2
mainaiko <mutantetrakor82@gmail.com>
2025-05-06 10:58:51 -0300

    atualizando

--- introduçao_a_aplicaçao_rest/verbos_HTTP.py
+++
@@ -1,5 +0,0 @@
-"""
-em APIs RESTful os verbos HTTP tem papeis especificos que se alinham com as operaçoes CRUD
-esta abordagem padronizada permite que as APIs sejam intuitivas e previsiveis facilitando a
-interaçao entre diferentes sistemas e aplicaçoes
-"""
